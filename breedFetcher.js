e894b22b6b88dea080e3cd83870012c23c834a61
harry thomas <harryt1905@gmail.com>
2022-09-29 16:36:51 -0400

    cat function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,14 @@
 let request = require('request');
-let breedQuery = process.argv.slice(2)[0];
-let fetchCatsByBreed = (breed) => {
-
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`,(error, response, body)=>{
-    if (error) return console.log("there aren't any cats today");
-    let bodyParsed = JSON.parse(body)[0];
-    if (!bodyParsed) return console.log('that is not a cat my friend, I would know');
-    console.log(`${bodyParsed.name}
-    Has a: ${bodyParsed.temperament} temperament
-    and originates from ${bodyParsed.origin}`);
+let fetchBreedDescription = (breed, callback) =>  {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`,(error,response, body) => {
+    if (error) return callback(error, null);
+    let desc = JSON.parse(body)[0];
+    if (desc) return callback(null, desc.description);
+    if (!desc.length) return callback('no cats', null);
   });
 };
+module.exports = fetchBreedDescription;
 
-fetchCatsByBreed(breedQuery);
+
+
 
